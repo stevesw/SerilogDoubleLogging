ed2c7513d00921241f5eb5c6f2be95879d1e28bb
Steven Wei <steve@hindsightlabs.com>
2021-08-17 14:23:33 -1000

    Initial commit.

---
+++ SerilogDoubleLogging/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SerilogDoubleLogging.Controllers
+{
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Test()
+        {
+            throw new Exception("Test");
+        }
+
+        [HttpGet]
+        [Route("error")]
+        public IActionResult Error()
+        {
+            return new JsonResult(new { message= "There was an error with your request." });
+        }
+    }
+}
---
+++ SerilogDoubleLogging/Startup.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace SerilogDoubleLogging
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseExceptionHandler("/error");
+
+            app.UseSerilogRequestLogging();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
